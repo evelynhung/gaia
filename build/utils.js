eb82689a4917588652cfdc618ef2e98e69936f09
Evelyn Hung <jj.evelyn@gmail.com>
2014-02-07 19:28:58 +0800

    Bug 957497's patch

--- build/utils.js
+++ build/utils.js
@@ -41,6 +41,15 @@ function ls(dir, recursive, exclude) {
   return results;
 }
 
+function isExternalApp(webapp) {
+  if (!webapp.metaData ||
+    (webapp.metaData && webapp.metaData.external === false)) {
+    return false
+  } else {
+    return true;
+  }
+}
+
 function getFileContent(file) {
   try {
     let fileStream = Cc['@mozilla.org/network/file-input-stream;1']
@@ -604,3 +613,4 @@ exports.readJSONFromPath = readJSONFromPath;
 exports.writeContentToFile = writeContentToFile;
 exports.processEvents = processEvents;
 exports.log = log;
+exports.isExternalApp = isExternalApp;
