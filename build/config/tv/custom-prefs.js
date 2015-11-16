5aba30a5ba908d67e324a2162287d443637cb9d5
Evelyn Hung <jj.evelyn@gmail.com>
2015-11-16 18:37:38 +0800

    Bug 1225048 - add default screen size

--- build/config/tv/custom-prefs.js
+++ build/config/tv/custom-prefs.js
@@ -2,6 +2,7 @@
 user_pref('devtools.responsiveUI.customWidth', 1366);
 user_pref('devtools.responsiveUI.customHeight', 768);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
+user_pref('b2g.screen.size', '1920x1080');
 user_pref('b2g.system_startup_url',
           'app://smart-system.gaiamobile.org/index.html');
 user_pref('b2g.system_manifest_url',
